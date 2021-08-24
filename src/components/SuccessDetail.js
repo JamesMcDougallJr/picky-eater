c464b3e920d1bdfb3383a56bff8a3620acb0b520
JamesMcDougallJr <jmcdouga@ucsd.edu>
2021-08-23 20:38:30 -0700

    Update with texting

--- src/components/SuccessDetail.js
+++ src/components/SuccessDetail.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity, Linking } from 'react-native';
+import GradientButton from 'react-native-gradient-buttons';
 import useYelpBusinessResults from '../hooks/useYelpBusinessResults';
 import StarRating from 'react-native-star-rating';
+
+import { phonecall, text } from 'react-native-communications';
 
 const SuccessDetail = ({ id }) => {
   const [name,
     rating,
     phone,
     imageUrl] = useYelpBusinessResults(id);
+
   return (
     <View>
       <Text style={styles.headingStyle}>{name}</Text>
-      <StarRating rating = {rating}/>
-      <TouchableOpacity onPress={() => Linking.openURL(`tel:${phone}`)}>
+      <StarRating rating={rating} />
+      <TouchableOpacity onPress={() => phonecall(phone, true)}>
         <Text style={styles.text}>Make a call: {phone}</Text>
       </TouchableOpacity>
-
+      <GradientButton
+        text="let James Know" 
+        onPressAction={() => text("9513311897", `Hey, let's go to ${name}!\n${imageUrl}`)} />
       <Image
         source={{ uri: imageUrl }}
         styles={styles.imageStyle} />
